76fbbc1dc24c20abbd7bef4da3eccaa5150a9053
Hannan Siddiqui <115110479+hannan-siddiqui@users.noreply.github.com>
2023-09-26 23:39:18 +0530

    Update admin.py

--- manager/admin.py
+++ manager/admin.py
@@ -6,4 +6,9 @@ from manager.models import landingpagecontent
 class managelandingpagecontent(admin.ModelAdmin):
     list_display=['img', 'imageheading', 'imagedex']
 
+class managebrandcontent(admin.ModelAdmin):
+    brandlist = ['brandimg', 'brandname']
+
+
 admin.site.register(landingpagecontent, managelandingpagecontent)
+admin.site.register(brandpagecontent, managebrandcontent)
