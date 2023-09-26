91762294be419fa59c198848a9015e7a4dfcced2
Hannan Siddiqui <115110479+hannan-siddiqui@users.noreply.github.com>
2023-09-26 23:37:55 +0530

    Update models.py

--- manager/models.py
+++ manager/models.py
@@ -6,5 +6,8 @@ class landingpagecontent(models.Model):
     img = models.ImageField(upload_to="upload/", verbose_name='')
     imageheading = models.CharField(max_length=50)
     imagedex = models.TextField()
-
+    
+class brandpagecontent(models.Model):
+    brandimg = models.ImageField(upload_to="upload/", verbose_name='')
+    brandname = models.CharField(max_length=20)
     
